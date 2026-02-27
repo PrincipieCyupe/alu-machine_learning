4263c4ee8d0f1069b98e293bf9f0817f5693d789
PrincipieCyupe <p.cyubahiro@alustudent.com>
2026-02-27 21:03:52 +0200

    final commit

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,24 @@
-#!/usr/bin/env python3
-import numpy as np
-import matplotlib.pyplot as plt
+# #!/usr/bin/env python3
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-y = np.arange(0, 11) ** 3
+# y = np.arange(0, 11) ** 3
 
-x = np.arange(0, 11)
-plt.plot(x, y, color="red")
-plt.show()
+# x = np.arange(0, 11)
+# plt.plot(x, y, color="red")
+# plt.show()
+# import pyttsx3
+# Text = input("Write any word: ")
+# a = pyttsx3.init()
+# b = a.say(Text)
+# a.runAndWait()
+
+def downward():
+    count = 10
+    for i in range(count):
+        count -= 1
+        print("*" * count)
+
+downward()
+
+
